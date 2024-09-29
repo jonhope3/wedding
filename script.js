a858fe487d4b19e69f6a84547fea57d7039c3092
Jon Hope <jonhope3@gmail.com>
2024-09-29 13:26:31 -0500

    Add style 1

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for mobile navigation toggle
-const mobileMenu = document.getElementById('mobile-menu');
-const navList = document.querySelector('.nav-list');
-
-// Toggle the active class on both the menu toggle and the nav-list
-mobileMenu.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active');
-    navList.classList.toggle('active');
-});
